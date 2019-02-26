ef32c0e921fe925c0d6dc0da969b6bf5dab720aa
carlaam <carla.am@outlook.com>
2019-02-26 15:24:24 -0300

    Versão 002 - Adicionando eventos do botão

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 
 window.onload = function(){
     showPokemons();
+    console.log(filterByType('Grass'));
+}
+
+function filterByType(typep){
+  const pokemonByType = getPokemons().filter((pokemon) => (pokemon["type"].includes(typep)))
+  return pokemonByType;
 }
 
 function getPokemons(){
     return POKEMON["pokemon"];
 }
-
 
 function showPokemons(){
     let pokemonDiv = document.querySelector('#pokemon-div');
@@ -23,3 +28,9 @@ function showPokemons(){
       `).join("")}
       `
    }
+
+
+
+
+
+
