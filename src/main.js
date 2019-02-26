4f20d0ee39c9c70dafeaf7f97b4a0ab90c1ab21f
Duda Duarte <dudammduarte@yahoo.com.br>
2019-02-26 15:18:11 -0300

    colocamos os filtros de tipo :DD

--- src/main.js
+++ src/main.js
@@ -1,16 +1,24 @@
-
-window.onload = function(){
+window.onload = function () {
     showPokemons();
 }
 
-function getPokemons(){
+let buttonsFilter = document.querySelectorAll('.type-pokemon');
+
+for (let button of buttonsFilter) {
+    button.addEventListener('click', function (event) {
+        filterPokemons(event.target.value);
+    })
+}
+
+
+function getPokemons() {
     return POKEMON["pokemon"];
 }
 
 
-function showPokemons(){
+function showPokemons() {
     let pokemonDiv = document.querySelector('#pokemon-div');
-  
+
     pokemonDiv.innerHTML = `  
     ${getPokemons().map((poke) => `
     <div class="single-pokemon">
@@ -22,4 +30,25 @@ function showPokemons(){
       </div>
       `).join("")}
       `
-   }
+}
+
+function filterPokemons(typep) {
+    const filter = getPokemons().filter((pokemon) => (pokemon["type"].includes(typep)));
+    showFilter(filter);
+}
+
+function showFilter(filter) {
+    let pokemonDiv = document.querySelector('#pokemon-div');
+
+    pokemonDiv.innerHTML = `  
+    ${filter.map((poke) => `
+    <div class="single-pokemon">
+      <img src="${poke["img"]}"
+      class="pokemon-img" />
+        <h4 class="select-num-pokemon">${poke["num"]}</h4>
+        <h3 class="pokemon-name">${poke["name"]}</h3>
+        <p class="select-type-pokemon">${poke["type"].join(', ')}</p>
+      </div>
+      `).join("")}
+      `
+}
