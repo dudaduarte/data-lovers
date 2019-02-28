4ec13f9df1909cc8ce7b583acec237c40d9a22e3
carlaam <carla.am@outlook.com>
2019-02-28 13:11:52 -0300

    Versao 4 - Adicionando ordenacao

--- src/main.js
+++ src/main.js
@@ -1,111 +1,107 @@
+
 window.onload = function () {
-  showPokemons();
-  getPokemonOnClik();
+  showPokemons(getPokemons());
 }
 
-
-function getPokemonOnClik (){
-  let pokemonList = document.querySelectorAll('.pokemon-img');
-
-  for(let pokemon of pokemonList){
-    pokemon.addEventListener('click', function(event){
-      console.log(event.target.src);
-      setPokemon(event.target.src);
-
-    })
-  }
-}
-
+let arrayResult = [];
 
 let buttonsFilter = document.querySelectorAll('.type-pokemon');
-
 
 for (let button of buttonsFilter) {
   button.addEventListener('click', function (event) {
     let type = event.target.value;
     if (type === "All") {
-      showPokemons()
+      showPokemons(getPokemons());
     } else {
       filterByType(type);
     }
-    getPokemonOnClik();
+    getPokemonOnClick();
   })
-  
 }
 
-document.querySelector('#input-name-pokemon').addEventListener('input', function(event) {
-  searchName(event.target.value);
+document.querySelector('#input-name-pokemon').addEventListener('input', function (event) {
+  filterByName(event.target.value);
 });
 
+document.querySelector('.order').addEventListener('change', function (event) {
+  if (event.target.value === 'a-to-z') {
+    sortByName();
+  } else if (event.target.value === 'z-to-a') {
+    showPokemons(arrayResult.reverse())
+  } else {
+    sortByNumber(event.target.value)
+  }
+})
+
 function getPokemons() {
-  return POKEMON["pokemon"];
+  arrayResult = POKEMON["pokemon"];
+  return arrayResult;
 }
 
-function showPokemons() {
-  let pokemonDiv = document.querySelector('#pokemon-div');
-
-  pokemonDiv.innerHTML = `  
-  ${getPokemons().map((poke) => `
-  <div class="single-pokemon">
-    <img src="${poke["img"]}"
-    class="pokemon-img" />
-      <h4 class="select-num-pokemon">${poke["num"]}</h4>
-      <h3 class="pokemon-name">${poke["name"]}</h3>
-      <p class="select-type-pokemon">${poke["type"].join(', ')}</p>
-    </div>
-    `).join("")}
-    `
+function filterByType(type) {
+  arrayResult = getPokemons().filter((pokemon) => (pokemon["type"].includes(type)));
+  showPokemons(arrayResult);
 }
 
-function filterByType(typep) {
-  const filter = getPokemons().filter((pokemon) => (pokemon["type"].includes(typep)));
-  showFilter(filter);
+function filterByName(input) {
+  const filteredByName = arrayResult.filter((pokemon) => (pokemon["name"].toLowerCase().includes(input.toLowerCase())));
+  showPokemons(filteredByName);
 }
 
-function showFilter(filter) {
-  let pokemonDiv = document.querySelector('#pokemon-div');
-
-  pokemonDiv.innerHTML = `  
-  ${filter.map((poke) => `
-  <div class="single-pokemon">
-    <img src="${poke["img"]}"
-    class="pokemon-img" />
-      <h4 class="select-num-pokemon">${poke["num"]}</h4>
-      <h3 class="pokemon-name">${poke["name"]}</h3>
-      <p class="select-type-pokemon">${poke["type"].join(', ')}</p>
-    </div>
-    `).join("")}
-    `
-
-
+function sortByName() {
+  arrayResult = (arrayResult.sort((a, b) =>
+    a.name.localeCompare(b.name))
+  );
+  showPokemons(arrayResult);
 }
 
-function searchName(input) {
-  const filterName = getPokemons().filter((pokemon) => (pokemon["name"].toLowerCase().includes(input.toLowerCase())));
-  showFilter(filterName);
+function sortByNumber(order) {
+  arrayResult = arrayResult.sort(function (a, b) {
+    return a.id - b.id;
+  })
+  if (order === 'decrescent') {
+    arrayResult = arrayResult.reverse()
+  }
+  showPokemons(arrayResult);
 }
 
+function getPokemonOnClik() {
+  let pokemonList = document.querySelectorAll('.pokemon-img');
+  for (let pokemon of pokemonList) {
+    pokemon.addEventListener('click', function (event) {
+      setPokemon(event.target.src);
+    })
+  }
+}
 
-function setPokemon (img){
+function setPokemon(img) {
   let pickedPokemon = document.querySelector('.picked');
   let div = document.querySelector('.picked-pokemon');
 
   div.innerHTML = "";
-  /*
-  if(!pickedPokemon.src){
-    pickedPokemon.src = img;
-  }
-  else{
-    pickedPokemon.removeAttribute(src);
-  }
-*/
 
   let image = document.createElement("img");
-  image.src=img;
+  image.src = img;
 
   image.classList.add("poke-teste");
 
   div.appendChild(image);
-
-  
 }
+
+function showPokemons(pokemonList) {
+  let pokemonDiv = document.querySelector('#pokemon-div');
+  pokemonDiv.innerHTML = `  
+  ${pokemonList.map((poke) => `
+  <div class="single-pokemon">
+    <img src="${poke["img"]}"
+    class="pokemon-img" />
+      <h4 class="select-num-pokemon">${poke["num"]}</h4>
+      <h3 class="pokemon-name">${poke["name"]}</h3>
+      <p class="select-type-pokemon">${poke["type"].join(', ')}</p>
+    </div>
+    `).join("")}
+    `
+  getPokemonOnClick();
+}
+
+
